e991480fc351f8958d7b7635bea85e96eb8d87f6
md gulam noxbondy <gulamnoxbondy@gmail.com>
2025-07-02 09:28:55 +0200

    workshop

---
+++ src/main/java/se/lexicon/workshopopenai27/WorkshopOpenAi27Application.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshopopenai27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopOpenAi27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopOpenAi27Application.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/workshopopenai27/WorkshopOpenAi27ApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.workshopopenai27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopOpenAi27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
